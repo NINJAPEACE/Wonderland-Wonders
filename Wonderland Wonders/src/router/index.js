2ac706a8b68ecefc00fe36901804b8ae273ddf66
NINJA PEACE <71932430+NINJAPEACE@users.noreply.github.com>
2024-05-09 07:27:46 +0000

    yes

--- Wonderland Wonders/src/router/index.js
+++ Wonderland Wonders/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TheHareQuestView from '../views/TheHareQuestView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: TheHareQuestView
+      component: HomeView
     },
     {
       path: '/theharequestyoushouldntknow',
